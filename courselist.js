8bd3e73c7f93336246b79e91da7881b619136a51
Constellatin39 <tina2877566057@gmail.com>
2020-04-19 20:44:05 +0800

    skip empty courses

--- courselist.js
+++ courselist.js
@@ -12,7 +12,11 @@ class courselist{
 		let built=[];
 		for(let i=0;i<courses.length;i++){
 			let course=courses.eq(i).find("a");
-			let target=url.parse(course.attr("href"));
+			let t_url = course.attr("href");
+			if(typeof t_url !== "string") {
+				continue;
+			}
+			let target=url.parse(t_url);
 			let query=qs.parse(target.query);
 			let title=course.attr("title");
 			built.push({courseId:query.courseId,clazzId:query.clazzid,title});
