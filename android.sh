5c69ca3febca43f69b8ba37d11f77b988ba6894e
lyj <2457395722@qq.com>
2022-04-03 15:25:38 +0800

    更新手机脚本

--- android.sh
+++ android.sh
@@ -1,9 +1,15 @@
 #/bin/bash
+updateAndRun(){
+   git pull
+   npm install
+   node main.js
+}
 
-if [  -d chaoxing-xuexitong-autoflush-master ]
+echo "android命令行一键脚本"
+if [  -d chaoxing-xuexitong-autoflush ]
 then
-   echo "脚本已经存在，直接运行"
-   node chaoxing-xuexitong-autoflush-master/main.js
+   echo "脚本已经存在，检查更新并运行"
+   updateAndRun
 else
 echo "脚本未下载，开始下载"
 sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/termux-packages-24 stable main@' $PREFIX/etc/apt/sources.list
@@ -11,21 +17,15 @@ apt update
 
 yes n | apt upgrade -y
 
-apt install nodejs wget unzip npm -y
+apt install nodejs wget unzip npm git -y
 
-wget -O cx.zip https://gh.fakev.cn/lyj0309/chaoxing-xuexitong-autoflush/archive/refs/heads/master.zip
+git clone https://gh.fakev.cn/lyj0309/chaoxing-xuexitong-autoflush.git
 
-unzip cx.zip
-
-rm cx.zip
-
-cd chaoxing-xuexitong-autoflush-master
+cd chaoxing-xuexitong-autoflush
 
 npm config set registry https://registry.npm.taobao.org/
 
-npm install
-
-node main.js
+updateAndRun
 fi   
 
 
